7ae3ce0e4a1e60ad11f6b2c270077152a1c2a2ea
Michael Buesch <m@bues.ch>
2017-03-09 21:33:09 +0100

    Remove obsolete xml tags
    Signed-off-by: Michael Buesch <m@bues.ch>

--- cms/cms.py
+++ cms/cms.py
@@ -1141,8 +1141,7 @@ class CMS(object):
 		pass
 
 	def __genHtmlHeader(self, title, additional = ""):
-		header = """<?xml version="1.0" encoding="UTF-8" ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+		header = """<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
