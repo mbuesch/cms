2e934f38e5d4728769d50f11bdb98ff6b1c0882d
Michael Buesch <m@bues.ch>
2019-08-03 19:40:40 +0200

    Add build script
    Signed-off-by: Michael Buesch <m@bues.ch>

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+basedir="$(dirname "$0")"
+[ "$(echo "$basedir" | cut -c1)" = '/' ] || basedir="$PWD/$basedir"
+
+set -e
+
+if ! [ -x "$basedir/setup.py" ]; then
+	echo "basedir sanity check failed"
+	exit 1
+fi
+
+cd "$basedir"
+
+rm -r build
+rm -f cms_cython
+python3 ./setup.py build
+ln -s build/lib.*-3.*/cms_cython .
